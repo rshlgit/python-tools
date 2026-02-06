c6e60e56a2b7be8aa0f325f7e87d5cb442e6d78b
Rushil <rshl1997@gmail.com>
2026-02-06 15:28:38 +0530

    upgrade analyzer to support stdin

--- analyzer.py
+++ analyzer.py
@@ -1,22 +1,23 @@
 import sys
 
-if len(sys.argv) != 2:
-    print("Usage: python3 analyzer.py <filename>")
-    sys.exit(1)
+def read_input():
+    if len(sys.argv) == 2:
+        filename = sys.argv[1]
+        try:
+             with open(filename, "r") as f:
+                 return f.read()
+        except FileNotFoundError:
+            print("Error: file not found:", filename)
+            sys.exit(1)
+    else:
+        return sys.stdin.read()
 
-filename = sys.argv[1]
-
-try:
-    with open(filename, "r") as f:
-        text = f.read()
-except FileNotFoundError:
-    print("Error: file not found:", filename)
-    sys.exit(1)
+text = read_input()
 
 lines = len(text.splitlines())
 words = len(text.split())
 chars = len(text)
 
-print(lines, words, chars, filename)
+print(lines, words, chars)
 
 
