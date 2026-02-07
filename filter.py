28b8697bc0f76bedbdf297e6cdae004fbe2988b7
Rushil <rshl1997@gmail.com>
2026-02-07 13:58:58 +0530

    add case-insensitive filter

--- filter.py
+++ filter.py
@@ -4,13 +4,13 @@ if len(sys.argv) != 3:
     print("Usage: python3 filter.py <word> <filename>")
     sys.exit(1)
 
-word = sys.argv[1]
-filename = sys.argv[2]
+word, filename = sys.argv[1:]
 
 try:
-    with open(filename) as f:
+    with open(filename, "r") as f:
         for line in f:
-            if word in line:
+            if word.lower() in line.lower():
                 print(line.strip())
 except FileNotFoundError:
-    print("Error: file not found: ", filename)
+    print("file  not found:", filename)
+
